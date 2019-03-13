48e627de590153bb3a094b087e255e2e3adba013
Jeremy Pedersen (曉美) <jeremydavispedersen@gmail.com>
2019-03-13 08:45:57 +0800

    Added a new script to launch Windows Server and install Chrome (on Alibaba Cloud). Useful when you just need remote desktop with internet access

--- README.md
+++ README.md
@@ -1,12 +1,75 @@
-# Terraform Examples
+Terraform Examples
+==================
 
-This repo is a personal collection of small Terraform examples and patterns. Feel free to use and modify any of the scripts you find here.
+This repository is a personal collection of small Terraform examples and patterns. Feel free to use and modify any of the scripts you find here.
 
 Currently, only scripts for Alibaba Cloud are available, but I plan to support all of these cloud platforms in the future:
 
 - Alibaba Cloud
-- Amaozn AWS
+- Amazon AWS
 - Google Cloud Platform
 - Microsoft Azure
 
+Structure
+=========
 
+Each example is built to be run independently of the others. Examples for each cloud provider are grouped into separate directories:
+
+- abc (Alibaba Cloud)
+- aws (Amazon AWS)
+- azure (Microsoft Azure)
+- gcp (Google Cloud Platform)
+
+Further, each example has its own subdirectory. Right now, some of these directories are missing, but eventually the tree should look something like this:
+
+```
+.
+├── LICENSE
+├── README.md
+├── abc
+│   └── chrome-on-windows
+│       ├── install_chrome.ps1
+│       ├── main.tf
+│       ├── outputs.tf
+|       └── README
+│       └── variables.tf
+│   └── scalable-wordpress
+│       ├── setup.sh
+│       ├── main.tf
+│       ├── outputs.tf
+|       └── README
+│       └── variables.tf
+├── aws
+│   └── chrome-on-windows
+│       ├── install_chrome.ps1
+│       ├── main.tf
+│       ├── outputs.tf
+|       └── README
+│       └── variables.tf
+│   └── scalable-wordpress
+│       ├── setup.sh
+│       ├── main.tf
+│       ├── outputs.tf
+|       └── README
+│       └── variables.tf
+├── azure
+│   └── chrome-on-windows
+    ...and so on
+
+```
+
+You should be able to descend into any project directory, like `aws/scalable-wordpress` and then simply run the scripts there like so:
+
+
+```
+terraform init
+terraform plan
+terraform apply
+```
+
+I won't necessarily provide the same set of scripts for each cloud provider, but I will try to do so wherever it's possible. 
+
+License
+=======
+
+This repository is licensed under "The Unlicense" so feel free to use the content here in any way you like.
---
+++ abc/chrome-on-windows/main.tf
@@ -0,0 +1,80 @@
+#
+# Set up a Windows Server ECS instance and install Chrome
+# using a PowerShell script
+#
+# Author: Jeremy Pedersen
+# Date: 2019-03-12
+#
+
+# Set up the "aliyun" (Alibaba Cloud) provider
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.access_key_secret}"
+  region     = "${var.region}"
+}
+
+# Determine what availability zones are available in our chosen region
+data "alicloud_zones" "abc_zones" {}
+
+# Find instances types available in the chosen region/availability zone
+# with at least 8 GB of memory
+data "alicloud_instance_types" "8g" {
+  memory_size       = 8
+  availability_zone = "${data.alicloud_zones.abc_zones.zones.0.id}"
+}
+
+# Create Windows Server ECS instance
+resource "alicloud_instance" "tf_example" {
+  instance_name = "tf_examples_windows2016"
+
+  image_id = "win2016_64_dtc_1607_en-us_40G_alibase_20181220.vhd"
+
+  instance_type        = "${data.alicloud_instance_types.8g.instance_types.0.id}"
+  system_disk_category = "cloud_efficiency"                                       # cheapest (standard) disk type
+  security_groups      = ["${alicloud_security_group.tf_example.id}"]
+
+  vswitch_id = "${alicloud_vswitch.tf_example.id}"
+
+  password = "${var.password}"
+
+  # Ensure we get a public IP address by choosing a non-zero Internet bandwidth
+  internet_max_bandwidth_out = 10 # 10 Mbps - plenty for a demo
+
+  # Powershell script to install Chrome
+  user_data = "${file("install_chrome.ps1")}"
+}
+
+# Set up vSwitch and VPC
+resource "alicloud_vpc" "tf_example" {
+  name       = "tf_examples_windows2016_vpc"
+  cidr_block = "${var.cidr_block}"
+}
+
+resource "alicloud_vswitch" "tf_example" {
+  vpc_id            = "${alicloud_vpc.tf_example.id}"
+  cidr_block        = "${var.cidr_block}"
+  availability_zone = "${data.alicloud_zones.abc_zones.zones.0.id}"
+}
+
+# Set up security group and security group rules
+resource "alicloud_security_group" "tf_example" {
+  name   = "tf_examples_windows2016"
+  vpc_id = "${alicloud_vpc.tf_example.id}"
+}
+
+# Create security group rule for RDP access
+resource "alicloud_security_group_rule" "tf_example_rdp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  port_range        = "3389/3389"
+  security_group_id = "${alicloud_security_group.tf_example.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "tf_example_outbound" {
+  type              = "egress"
+  ip_protocol       = "all"
+  port_range        = "-1/-1"
+  security_group_id = "${alicloud_security_group.tf_example.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
