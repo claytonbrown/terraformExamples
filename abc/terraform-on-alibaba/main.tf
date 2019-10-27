52f89d28cbe9fce49512cd5250e25c27f7514107
Jeremy Pedersen <jeremydavispedersen@gmail.com>
2019-10-27 11:10:31 +0800

    Updated diagrams and TF provider version for terraform-on-alibaba script

--- abc/terraform-on-alibaba/main.tf
+++ abc/terraform-on-alibaba/main.tf
@@ -4,7 +4,7 @@ provider "alicloud" {
   access_key = "${var.access_key}"
   secret_key = "${var.access_key_secret}"
   region     = "${var.region}"
-  version    = "~> 1.55"
+  version    = "~> 1.58"
 }
 
 data "alicloud_zones" "abc_zones" {}
