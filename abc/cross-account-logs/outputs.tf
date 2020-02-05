c18065cca645561a3b91b537f0acef5c30797b3f
曉美 <xiaomei@ali-98e0d984978d-2.local>
2020-02-05 09:52:09 +0800

    Added new cross account logging example code

---
+++ abc/cross-account-logs/outputs.tf
@@ -0,0 +1,13 @@
+#
+# Output the information required to test the
+# environment created in main.tf
+#
+
+output "root_uid" {
+  description = "UID of root account"
+  value       = "${var.root_uid}"
+}
+
+output "other_uid" {
+  description = "UID of other account"
+}
